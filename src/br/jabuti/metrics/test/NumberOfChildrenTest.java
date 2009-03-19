100419a661639bb3a5106078f32dbacafe1ea668
prmmeirelles <prmmeirelles@e3a74688-0f11-11de-9004-1fff2a6de87b>
2009-03-19 14:59:36 +0000

    Refactoring dos testes de Numeros de filhos de uma classe: alterando estilo do codigo para nomes em ingles; Comentou a string de depuracao na classe Program, que escrevia o classpath.

---
+++ src/br/jabuti/metrics/test/NumberOfChildrenTest.java
@@ -0,0 +1,119 @@
+/**
+ * 
+ */
+package br.jabuti.metrics.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.jabuti.lookup.Program;
+import br.jabuti.metrics.Metrics;
+
+/**
+ * @author paulo and raphael
+ *
+ */
+public class NumberOfChildrenTest {
+
+	String inputClassName;
+	
+	Program programTest;
+	Metrics metrics;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	
+	@Test
+	public void tryToLoadNonExistingClass(){
+		inputClassName = "ClasseQueNaoExiste"; 
+		try {
+			programTest = new Program(inputClassName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		metrics = new Metrics(programTest);
+		double numeroDeFilhos = metrics.noc(inputClassName);
+		assertEquals(-1,numeroDeFilhos,0);
+	}
+	
+	@Test
+	public void numberOfChildrenEqualsZeroTest(){
+		inputClassName = "br.jabuti.metrics.test.testInput.InputTesteNumeroDeFilhosIgual0"; // Eh necessario que seja passado o nome inteiro da classe (classe + pacotes);
+		try {
+			programTest = new Program(inputClassName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		metrics = new Metrics(programTest);
+		double numeroDeFilhos = metrics.noc(inputClassName);
+		assertEquals(0,numeroDeFilhos,0);
+		
+	}
+	
+	@Test
+	public void loadChildrenTest(){
+		inputClassName = "br.jabuti.metrics.test.testInput.InputTesteNumeroDeFilhosIgual1"; // Eh necessario que seja passado o nome inteiro da classe (classe + pacotes);
+		try {
+			programTest = new Program(inputClassName);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int numberOfLoadedClasses = programTest.getClasses().length;
+		assertEquals(2, numberOfLoadedClasses);
+		assertEquals(inputClassName, programTest.getClasses()[1]);
+		
+	}
+	
+	
+	@Test
+	public void numberOfChildrenEqualsOneTest(){
+
+		inputClassName = "br.jabuti.metrics.test.testInput.InputTesteNumeroDeFilhosIgual1"; // Eh necessario que seja passado o nome inteiro da classe (classe + pacotes);
+		try {
+			programTest = new Program(inputClassName);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		metrics = new Metrics(programTest);
+		double numeroDeFilhos = metrics.noc(inputClassName);
+		assertEquals(1,numeroDeFilhos,0);
+	}
+	
+	
+	
+
+}
+
